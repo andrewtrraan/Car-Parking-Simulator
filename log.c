f34eb0389e8aa1f8ce1183ed8ede5c5efeeb0752
abdulm-shk <31016254+abdulm-shk@users.noreply.github.com>
2017-11-30 20:15:46 -0500

    Update log.c

--- log.c
+++ log.c
@@ -1,15 +1,29 @@
-//#include <iostream>
+#include <stdio.h>
 #include <time.h>
-//using namespace std;
 
-void timestamp()
+const char * timestamp()
 {
 	time_t ltime; /* calendar time */
 	ltime=time(NULL); /* get current cal time */
 	printf("%s",asctime( localtime(&ltime) ) );
+	return asctime(localtime(&ltime));
+}
+
+int writeLog(const char filename[], const char severity[], const char description[]){
+	FILE *log = NULL;
+	log = fopen(filename, "a");
+	if (log == NULL)
+    {
+        printf("Error! can't open log file.");
+        return -1;
+    }
+
+    fprintf(log, "%s%s %s\n", timestamp(), severity, description);
+    fclose(log);
+		return 0;
 }
 
 int main() {
-		timestamp();
+    writeLog("log.txt", "FATAL", "Error: not working");
     return 0;
 }
